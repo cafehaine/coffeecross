3652bda61cf98e788ad9e559aa2dbb43ab266618
cafehaine <kilian.guillaume@gmail.com>
2019-12-30 18:13:28 +0100

    Add lowercase characters to the font

--- coffeecross/lib/gui/utils.lua
+++ coffeecross/lib/gui/utils.lua
@@ -1,6 +1,6 @@
 local m = {}
 
-local GLYPHS = [[ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789]]
+local GLYPHS = [[ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789abcdefghijklmnopqrstuvwxyz]]
 local FONT = love.graphics.newImageFont("assets/font.png", GLYPHS)
 
 function m.get_font()
