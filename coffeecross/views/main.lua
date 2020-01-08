c74ea3f5ace60f01fe9ed5722f162b788da11a4b
cafehaine <kilian.guillaume@gmail.com>
2020-01-08 12:57:08 +0100

    Add link to github in main view
    Closes #1

--- coffeecross/views/main.lua
+++ coffeecross/views/main.lua
@@ -6,7 +6,7 @@ local gui = {
 		{type="background"},
 		{
 			group_type = "grid_row",
-			grid_layout = {"auto", 1},
+			grid_layout = {"auto", 1, "auto"},
 			elements = {
 				{
 					group_type = "grid_column",
@@ -15,21 +15,21 @@ local gui = {
 						{type="image", image="logo.png", mode="contain"},
 						{
 							id=1,
-							focus={up=3,down=2},
+							focus={up=3,down=2,left="gh",right="gh"},
 							type="button",
 							text="Levels",
 							action=function()viewstack.pushnew("world_selector")end
 						},
 						{
 							id=2,
-							focus={up=1,down=3},
+							focus={up=1,down=3,left="gh",right="gh"},
 							type="button",
 							text="Settings",
 							action=function()viewstack.pushnew("settings")end
 						},
 						{
 							id=3,
-							focus={up=2,down=1},
+							focus={up=2,down=1,left="gh",right="gh"},
 							type="button",
 							text="Exit",
 							action=viewstack.pop
@@ -37,7 +37,21 @@ local gui = {
 						{type="text",text="CoffeeCross, alpha 0.1"}
 					}
 				},
-				{type="none"}
+				{type="none"},
+				{
+					group_type="grid_column",
+					grid_layout={1,"auto"},
+					elements={
+						{type="none"},
+						{
+							id="gh",
+							focus={left=1, right=1},
+							type="button",
+							text="Github",
+							action=function()love.system.openURL("https://github.com/cafehaine/coffeecross")end
+						}
+					}
+				}
 			}
 		}
 	}
