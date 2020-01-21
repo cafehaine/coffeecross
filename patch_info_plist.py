04d9806135d211796ccd8fbee1558887d1476cf7
cafehaine <kilian.guillaume@gmail.com>
2020-01-21 08:50:19 +0100

    MacOs: Actually patch Info.plist

--- patch_info_plist.py
+++ patch_info_plist.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-
-lines = []
+from plistlib import dump, load, FMT_XML
 
 path = "build/tmp/love.app/Contents/Info.plist"
 
 print("Reading Info.plist...")
 
-with open(path) as data:
-	for line in data:
-		lines.append(line.rstrip())
+with open(path, 'rb') as fp:
+    plist = load(fp, fmt=FMT_XML)
 
 print("Patching data...")
-#TODO see https://love2d.org/wiki/Game_Distribution#Creating_a_macOS_Application
+# see https://love2d.org/wiki/Game_Distribution#Creating_a_macOS_Application
+plist['CFBundleIdentifier'] = 'eu.cafehaine.coffeecross'
+plist['CFBundleName'] = 'CoffeeCross'
+plist.pop('UTExportedTypeDeclarations', None)
 
 print("Writing patched data...")
 
-with open(path, 'w') as out:
-	out.write("\n".join(lines))
+with open(path, 'wb') as fp:
+	dump(plist, fp, fmt=FMT_XML)
 
 print("Done.")
