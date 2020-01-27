b0f5b170b3fe1ff362643129b29cf702a2615744
cafehaine <kilian.guillaume@gmail.com>
2020-01-27 16:35:29 +0100

    Fix shader execution on android

--- coffeecross/assets/shaders/dotted_line.glsl
+++ coffeecross/assets/shaders/dotted_line.glsl
@@ -2,12 +2,10 @@ uniform float time;
 
 vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
 	// In-game unit
-	float unit = min(love_ScreenSize.x, love_ScreenSize.y) / 100;
+	float unit = min(love_ScreenSize.x, love_ScreenSize.y) / 100.;
 	vec4 texcolor = Texel(tex, texture_coords);
-	//return texcolor * color;
-	float test = mod(screen_coords.x+screen_coords.y + time*unit*5, unit*6)/(unit*3);
-	if (test >=1)
+	float test = mod(screen_coords.x+screen_coords.y + time*unit*5., unit*6.)/(unit*3.);
+	if (test >=1.)
 		return color;
 	return vec4(0, 0, 0, 1);
-	//return vec4(mod(screen_coords[0], unit*2)/unit, mod(screen_coords[1], unit)/unit, 0, 1);
 }
